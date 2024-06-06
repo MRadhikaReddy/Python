2f67de70d7593baccd12a412c91b898bf1375aa4
Radhika Reddy <mandipalliradhikareddy@gmail.com>
2024-06-06 21:38:11 +0530

    first commit

---
+++ new.program.py
@@ -0,0 +1,103 @@
+# a = 65
+
+# ascii_a = chr(a)
+# print(ascii_a)
+
+# a = 10
+# b = 20
+ 
+# # a = a/b
+# # a!=b
+# result_not_equal = a>=b
+# print(a)
+
+# print(10+ 20 * 30)
+
+# name = input("Enter your name:")
+# age = int(input("Enter your age:"))
+
+# print("Hello," +name+"!you are",age,"years old.")
+
+# a = int(input("Give a value:"))
+# b = int(input("Give b value:"))
+# print(a,b,sep="-",end="Ended here")
+
+# name = str(input("Enter your name:"))
+
+# print ("Hello",name,sep=", ",end="!")
+
+# a = float(input("You entered:"))
+
+# print ("Value of Pi",a ,sep=":")
+
+# a = int(input("Give a value:"))
+# b = int(input("Give b value:"))
+# c = int(input("Give c value:"))
+
+# s = a+b+c
+
+# print("sum of Inputs",s,sep=":")
+
+# a = input()
+# x,y,z = a.split(" ")
+# sum = int(x) + int(y) + int(z)
+# print(sum)
+
+# inp = input("Enter name and age: ")
+# name,age = inp.split(",")
+# print("Name:",name,",Age:",age,sep="")
+
+# n = int(input("Enter n:"))
+# print("Countdown: 5 4 3 2 1", end = "Blast off!")
+
+# x,y = input("Enter a and b values: ").split(",")
+# a = int(x)
+# b = int(y)
+# print("Addition:",a+b, ",Subtraction:",a-b, ",Multiplication:",a*b, ",Division:",a/b ,sep="")
+
+# a = input("Enter a value:")
+# print("10>5:",10>5,",10<5:",10<5,",10==5:",10==5,",10!=5:",10!=5,sep="")
+
+# num1 = int(input())
+# num2 = int(input())
+
+# print("Sum:",(num1+num2))
+
+# radius = int(input("Enter radius value:"))
+
+# print("Area of the circle:",3.14*radius*radius)
+
+# a = input("Enter a value:")
+# print("True and False:",False,",True or False:",True,",not True:",False,sep="")
+
+# a =int(input("Enter a value:"))
+# print("You entered:", Yes)
+
+# user_input = input("Please enter 'yes':")
+# if user_input.lower()=="yes":
+#     print("You entered: Yes")
+#     else:
+#         print("print's not 'yes'. please enter 'yes'.")
+
+
+# a =int(input("Enter a:"))
+# b =int(input("Enter b:"))
+# c =int(input("Enter c:"))
+
+# root1 = 0
+# root2 = 0
+
+# d =(b**2)-4*a*c
+# root1 = (-b+(d**(0.5)))/2*a
+# root2 = (-b-(d**(0.5)))/2*a
+# print(f"Roots:({root1},{root2})")
+
+
+# print([x for x in range(2, 201) if all(x % y != 0 for y in range(2, int(x**0.5) + 1))])
+
+weather ="Sunny"
+
+if weather == "Bunny":
+    print("You can play with your fav toys!")
+
+print("Have fun!")
